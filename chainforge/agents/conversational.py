ec77df73af9038d31aa3d8e39535330c153cc903
xiaobao <xiaobao@fuse.co.id>
2026-07-14 17:14:07 +0800

    fix: correct copyright year to 2026

--- chainforge/agents/conversational.py
+++ chainforge/agents/conversational.py
@@ -1,4 +1,4 @@
-# Copyright 2024 ChainForge Contributors
+# Copyright 2026 ChainForge Contributors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
